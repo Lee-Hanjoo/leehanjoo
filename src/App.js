65e32a3bc6bfe402194caef3f6f2772be445ad17
insu <dlstn7609@hanmail.net>
2024-05-29 12:05:30 +0900

    visual

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/reset.css';
+import './css/style.css';
 
 function App() {
-  return (
-    <div id="lhj">
-      안녕
-    </div>
-  );
+    return (
+        <main>
+            <div className='visual'>
+                <ul class="star">
+                    <li>
+                        <img src="./img/star.svg" alt="" />
+                    </li>
+                    <li class="on">
+                        <img src="./img/star.svg" alt="" />
+                    </li>
+                    <li>
+                        <img src="./img/star.svg" alt="" />
+                    </li>
+                </ul>
+                <ul class="txt lang">
+                    <li>Html5 Css3</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                </ul>
+                <p class="txt portfolio">Lee Han joo Portfolio</p>
+                <p class="txt dev">Front-end.Dev</p>
+                <ul class="txt name">
+                    <li>L</li>
+                    <li>H</li>
+                    <li>J</li>
+                </ul>
+            </div>
+        </main>
+    );
 }
 
 export default App;
