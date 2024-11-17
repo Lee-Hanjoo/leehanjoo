b45716a4cd15adb54a6b4a9649f80fd53674a8d2
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-11-17 23:12:00 +0900

    board url fix

--- src/App.js
+++ src/App.js
@@ -209,7 +209,7 @@ function App() {
                                 title="Board"
                                 desc={`게시판 프로젝트입니다.\n풀스택 웹 페이지입니다.`}
                                 info={`Next, MongoDB, Ajax, Auth, JWT, AWS`}
-                                webLink='http://leehanjoo3.dothome.co.kr/'
+                                webLink='http://leehanjooboard.ap-northeast-2.elasticbeanstalk.com/'
                             />
                         </Box>
                         <Box
