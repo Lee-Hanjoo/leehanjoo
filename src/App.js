362d203e48449e1b4292bacd9fc695a79eebf6fa
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-11-24 00:08:33 +0900

    board url

--- src/App.js
+++ src/App.js
@@ -207,8 +207,7 @@ function App() {
                                 title="Board"
                                 desc={`게시판 프로젝트입니다.\n풀스택 웹 페이지입니다.`}
                                 info={`Next, MongoDB, Ajax, Auth, JWT, AWS`}
-                                // webLink='http://boardhanjoo-env.eba-5v3b6bph.ap-northeast-2.elasticbeanstalk.com/'
-                                webLink='https://www.figma.com/design/0pyVBvtKKZJcTDUJziDXOu/board?node-id=0-1&t=OdzficjtuwBBv20X-1'
+                                webLink='http://boardhanjoo-env.eba-5v3b6bph.ap-northeast-2.elasticbeanstalk.com/'
                             />
                         </Box>
                         <Box
