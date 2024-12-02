0196f32a2096d61d81ffd60e3cf9e30d5255a93d
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-12-02 23:01:15 +0900

    board

--- src/App.js
+++ src/App.js
@@ -206,7 +206,7 @@ function App() {
                                 sub="Web Project [진행중...]"
                                 title="Board"
                                 desc={`게시판 프로젝트입니다.\n풀스택 웹 페이지입니다.`}
-                                info={`Next, MongoDB, Ajax, Auth, JWT, AWS`}
+                                info={`Next, MongoDB, Ajax, Auth, JWT, AWS, Vercel`}
                                 webLink='https://board-ruddy-nu.vercel.app/'
                             />
                         </Box>
