04932c11afdbcd7f4b527064df6e5c64c6af8756
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-12-02 22:50:34 +0900

    url fix

--- src/App.js
+++ src/App.js
@@ -207,7 +207,7 @@ function App() {
                                 title="Board"
                                 desc={`게시판 프로젝트입니다.\n풀스택 웹 페이지입니다.`}
                                 info={`Next, MongoDB, Ajax, Auth, JWT, AWS`}
-                                webLink='http://lhjboard-env.ap-northeast-2.elasticbeanstalk.com/'
+                                webLink='https://board-ruddy-nu.vercel.app/'
                             />
                         </Box>
                         <Box
